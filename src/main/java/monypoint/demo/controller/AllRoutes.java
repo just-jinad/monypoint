92fde41962e1be6dc3185ddfa15d9bead45b1659
Jinad Tope Isaac <jinadtope66@gmail.com>
2025-05-28 09:51:15 +0100

    updated the monypoint project

--- src/main/java/monypoint/demo/controller/AllRoutes.java
+++ src/main/java/monypoint/demo/controller/AllRoutes.java
@@ -1,13 +1,13 @@
 package monypoint.demo.controller;
 
 import monypoint.demo.entity.User;
-import monypoint.demo.entity.Account;
+// import monypoint.demo.entity.Account;
 import monypoint.demo.service.UserService;
 import monypoint.demo.service.VerificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+// import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
